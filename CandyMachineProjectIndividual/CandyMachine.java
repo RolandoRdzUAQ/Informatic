c800423e66c6e64fd06bd72065c25f714c9fb517
Rolando Rodriguez <rodriguez.castro.rolando@gmail.com>
2026-04-08 21:05:50 -0600

    CandyMachine Add

---
+++ CandyMachineProjectIndividual/CandyMachine.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class CandyMachine {
+    
+    static Scanner console = new Scanner(System.in);
+
+    public static void showSelection() {
+        System.out.println("*** Welcome to Shelly's Candy Shop ***");
+        System.out.println("To select an item, enter ");
+        System.out.println("1 for Candy");
+        System.out.println("2 for Chips");
+        System.out.println("3 for Gum");
+        System.out.println("4 for Cookies");
+        System.out.println("9 to exit");
+    }
+
+}
